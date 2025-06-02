2e5977cbaa0c510d4abd63981ee1c2ce3fc8e230
evghjrnhvghrj <qwertyuiolkjhgfcdxszxcvbhnygwefuw74387t437@outlook.com>
2025-06-02 13:54:39 -0700

    Create firebase-init.js

---
+++ public/firebase-init.js
@@ -0,0 +1,11 @@
+// firebase-init.js
+const firebaseConfig = {
+  apiKey: " AIzaSyB3X6NyiQg5bcJqKvEB9bB_0Xvp1JFiEiM",
+  authDomain: "uion-461720.firebaseapp.com",
+  projectId: "uion-461720",
+  storageBucket: "uion-461720.appspot.com",
+  messagingSenderId: "XXXXXXX",
+  appId: "XXXXXXXXXX"
+};
+
+firebase.initializeApp(firebaseConfig);
